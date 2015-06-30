943693e200ad87b29d783804787ea737e911261c
freialobo <freialisa@mgail.com>
2015-06-29 21:07:10 -0700

    hello world, meet clicky

---
+++ clicky.js
@@ -0,0 +1,23 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.clicky.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.clicky.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
